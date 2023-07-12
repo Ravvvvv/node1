7bf401a1089c1573b4a3180490f479cb98bc79a8
rafal A <rafalandrzejczuk1@gmail.com>
2023-07-12 21:10:05 +0200

    commit21

--- index.js
+++ index.js
@@ -8,11 +8,53 @@ fs.readdir(path.join(__dirname, 'data'), function (err, files) {
     } else {
         console.log(files);
 
-        files.forEach(function (file) {
-            fs.readFile()
-        })
+        fs.mkdir(path.join(__dirname, 'average'), function (err) {
+            if (err) {
+                if (err.code === 'EEXIST') {
+                    console.log('Folder juz istnieje');
+                }
+                console.log(err);
+
+            } else {
+                console.log('stworzono folder');
+            }
+        });
+
+
+
+        files.forEach(function (file, index) {
+
+            fs.readFile(path.join(__dirname, 'data', file), 'utf8', function (err, data) {
+
+                let sum = 0;
+                let studentsCount = 0;
+
+                if (err) {
+                    console.log(err);
+                } else {
+
+                    let studentsData = JSON.parse(data);
+
+                    for (let studentData of studentsData) {
+                        sum += studentData.mark;
+                        studentsCount++;
+                    }
+
+                }
+
+                console.log(sum);
+
+                fs.writeFile(path.join(__dirname, 'average', 'result-year-' + (index + 1) + '.txt'), (sum / studentsCount).toString(), function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('Stworzono plik');
+                    }
+                })
+
+            });
+        });
     }
-})
+});
 
 
-console.log();
