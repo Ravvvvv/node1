e9b21f583c1bd0f8959a0a420a90370c597a1a31
rafal A <rafalandrzejczuk1@gmail.com>
2023-07-12 16:53:15 +0200

    commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
-const path = ('path') ;
+const path = require('path');
 
 
-fs.readdir(path.join(__dirname, 'data'),function(err,files){
- if(err){
-    console.log(err);
- } else{
-    console.log(files);
- }
-} )
+fs.readdir(path.join(__dirname, 'data'), function (err, files) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(files);
+
+        files.forEach(function (file) {
+            fs.readFile()
+        })
+    }
+})
